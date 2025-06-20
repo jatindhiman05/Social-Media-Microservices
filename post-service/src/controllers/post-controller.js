a6cf43c95699a11248164ab955dc5b7f4229821d
jatinn27 <jatin121dhiman@gmail.com>
2025-06-21 04:33:14 +0530

    media service search service

--- post-service/src/controllers/post-controller.js
+++ post-service/src/controllers/post-controller.js
@@ -1,52 +1,60 @@
-const Post = require('../models/Post');
-const logger = require('../utils/logger');
-const { validateCreatePost } = require('../utils/validation');
+const Post = require("../models/Post");
+const logger = require("../utils/logger");
+const { publishEvent } = require("../utils/rabbitmq");
+const { validateCreatePost } = require("../utils/validation");
 
-async function invalidatePostCache(req,input){
-    const cacheKey = `poat:${input}`
-    await req.redisClient.del(cacheKey);
-    const keys = await req.redisClient.keys('posts:*');
-    if(keys.length > 0){
+async function invalidatePostCache(req, input) {
+    const cachedKey = `post:${input}`;
+    await req.redisClient.del(cachedKey);
+
+    const keys = await req.redisClient.keys("posts:*");
+    if (keys.length > 0) {
         await req.redisClient.del(keys);
     }
 }
 
 const createPost = async (req, res) => {
-    logger.info('Create Post Endpoint hit ..')
+    logger.info("Create post endpoint hit");
     try {
-        // validate the schema
-        const { error } = validateCreatePost(req.body)
+        //validate the schema
+        const { error } = validateCreatePost(req.body);
         if (error) {
-            logger.warn('Validation error', error.details[0].message)
+            logger.warn("Validation error", error.details[0].message);
             return res.status(400).json({
                 success: false,
                 message: error.details[0].message,
-            })
+            });
         }
         const { content, mediaIds } = req.body;
-
-        const newPost = await Post.create({
+        const newlyCreatedPost = new Post({
             user: req.user.userId,
             content,
-            mediaIds: mediaIds || []
-        })
+            mediaIds: mediaIds || [],
+        });
 
-        await newPost.save();
-        await invalidatePostCache(req,newPost._id.toString());
+        await newlyCreatedPost.save();
 
-        logger.info('Post Created Successfully', newPost)
+        await publishEvent("post.created", {
+            postId: newlyCreatedPost._id.toString(),
+            userId: newlyCreatedPost.user.toString(),
+            content: newlyCreatedPost.content,
+            createdAt: newlyCreatedPost.createdAt,
+        });
+
+        await invalidatePostCache(req, newlyCreatedPost._id.toString());
+        logger.info("Post created successfully", newlyCreatedPost);
         res.status(201).json({
             success: true,
-            message: 'Post Created Successfully'
-        })
-    } catch (error) {
-        logger.error("Error Creating Post", error);
+            message: "Post created successfully",
+        });
+    } catch (e) {
+        logger.error("Error creating post", error);
         res.status(500).json({
             success: false,
-            message: "Error Creating Post"
-        })
+            message: "Error creating post",
+        });
     }
-}
+};
 
 const getAllPosts = async (req, res) => {
     try {
@@ -58,89 +66,103 @@ const getAllPosts = async (req, res) => {
         const cachedPosts = await req.redisClient.get(cacheKey);
 
         if (cachedPosts) {
-            return res.json(JSON.parse(cachedPosts))
+            return res.json(JSON.parse(cachedPosts));
         }
 
-        const posts = await Post.find({}).sort({ createdAt: -1 }).skip(startIndex).limit(limit);
+        const posts = await Post.find({})
+            .sort({ createdAt: -1 })
+            .skip(startIndex)
+            .limit(limit);
 
-        const totalNoofPosts = await Post.countDocuments();
+        const totalNoOfPosts = await Post.countDocuments();
 
         const result = {
             posts,
-            currentpage : true,
-            totalPages: Math.ceil(totalNoofPosts/limit),
-            totalPosts : totalNoofPosts
-        }
+            currentpage: page,
+            totalPages: Math.ceil(totalNoOfPosts / limit),
+            totalPosts: totalNoOfPosts,
+        };
 
-        // save your posts in redis cache
-        await req.redisClient.setex(cacheKey,300,JSON.stringify(result));
+        //save your posts in redis cache
+        await req.redisClient.setex(cacheKey, 300, JSON.stringify(result));
 
         res.json(result);
-    } catch (error) {
-        logger.error("Error Fetching Posts", error);
+    } catch (e) {
+        logger.error("Error fetching posts", error);
         res.status(500).json({
             success: false,
-            message: "Error Fetching Posts"
-        })
+            message: "Error fetching posts",
+        });
     }
-}
+};
 
 const getPost = async (req, res) => {
     try {
         const postId = req.params.id;
-        const cacheKey = `poar:${postId}`;
-
-        const cachedPost = await req.redisClient.get(cacheKey);
+        const cachekey = `post:${postId}`;
+        const cachedPost = await req.redisClient.get(cachekey);
 
         if (cachedPost) {
-            return res.json(JSON.parse(cachedPost))
+            return res.json(JSON.parse(cachedPost));
         }
 
-        const singlePostDetailsbyID = await Post.findById(postId);
+        const singlePostDetailsbyId = await Post.findById(postId);
 
-        if(!singlePostDetailsbyID){
+        if (!singlePostDetailsbyId) {
             return res.status(404).json({
-                message : 'Post not found',
-                success :false
-            })
+                message: "Post not found",
+                success: false,
+            });
         }
 
-        await req.redisClient.setex(cachedPost,3600,JSON.stringify(singlePostDetailsbyID))
+        await req.redisClient.setex(
+            cachedPost,
+            3600,
+            JSON.stringify(singlePostDetailsbyId)
+        );
 
-        res.json(singlePostDetailsbyID)
-    } catch (error) {
-        logger.error("Error Fetching Post", error);
+        res.json(singlePostDetailsbyId);
+    } catch (e) {
+        logger.error("Error fetching post", error);
         res.status(500).json({
             success: false,
-            message: "Error Fetching Post By ID"
-        })
+            message: "Error fetching post by ID",
+        });
     }
-}
+};
 
 const deletePost = async (req, res) => {
     try {
         const post = await Post.findOneAndDelete({
-            _id : req.params.id,
-            user : req.user.userId
-        })
+            _id: req.params.id,
+            user: req.user.userId,
+        });
+
         if (!post) {
             return res.status(404).json({
-                message: 'Post not found',
-                success: false
-            })
+                message: "Post not found",
+                success: false,
+            });
         }
 
-        await invalidatePostCache(req,req.params.id);
+        //publish post delete method ->
+        await publishEvent("post.deleted", {
+            postId: post._id.toString(),
+            userId: req.user.userId,
+            mediaIds: post.mediaIds,
+        });
+
+        await invalidatePostCache(req, req.params.id);
         res.json({
-            message : 'Post deleted Successfully'
-        })
-    } catch (error) {
-        logger.error("Error deleting Post", error);
+            message: "Post deleted successfully",
+        });
+    } catch (e) {
+        logger.error("Error deleting post", error);
         res.status(500).json({
             success: false,
-            message: "Error deleting"
-        })
+            message: "Error deleting post",
+        });
     }
-}
+};
 
-module.exports = { createPost, getAllPosts, getPost, deletePost }
+module.exports = { createPost, getAllPosts, getPost, deletePost };
--- post-service/src/server.js
+++ post-service/src/server.js
@@ -1,54 +1,66 @@
-require('dotenv').config()
-const express = require('express');
-
-const mongoose = require('mongoose');
-const Redis = require('ioredis');
-const cors = require('cors');
-const helmet = require('helmet');
-const postRoutes = require('./routes/post-routes')
-const errorHandler = require('./middleware/errorHandler')
-const logger = require('./utils/logger')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Redis = require("ioredis");
+const cors = require("cors");
+const helmet = require("helmet");
+const postRoutes = require("./routes/post-routes");
+const errorHandler = require("./middleware/errorHandler");
+const logger = require("./utils/logger");
+const { connectToRabbitMQ } = require("./utils/rabbitmq");
 
 const app = express();
-
 const PORT = process.env.PORT || 3002;
 
-mongoose.connect(process.env.MONGODB_URI)
-
+//connect to mongodb
 mongoose
     .connect(process.env.MONGODB_URI)
-    .then(() => logger.info('Connected to MongoDb'))
-    .catch(e => logger.error('Mongo Connection error', e));
+    .then(() => logger.info("Connected to mongodb"))
+    .catch((e) => logger.error("Mongo connection error", e));
 
-const redisClient = new Redis(process.env.REDIS_URL)
+const redisClient = new Redis(process.env.REDIS_URL);
 
-app.use(express.json());
-app.use(cors());
+//middleware
 app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.use((req, res, next) => {
-    logger.info(`Received ${req.method} request to ${req.url}`)
-    logger.info(`Request body, ${req.body}`)
+    logger.info(`Received ${req.method} request to ${req.url}`);
+    logger.info(`Request body, ${req.body}`);
     next();
-})
+});
 
-// *** homework 
+//*** Homework - implement Ip based rate limiting for sensitive endpoints
 
-// pass redis client to routes
-app.use('/api/posts',(req,res,next)=>{
-    req.redisClient = redisClient
-    next()
-},postRoutes)
-
+//routes -> pass redisclient to routes
+app.use(
+    "/api/posts",
+    (req, res, next) => {
+        req.redisClient = redisClient;
+        next();
+    },
+    postRoutes
+);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    logger.info(`Identity service running on port ${PORT}`)
-})
+async function startServer() {
+    try {
+        await connectToRabbitMQ();
+        app.listen(PORT, () => {
+            logger.info(`Post service running on port ${PORT}`);
+        });
+    } catch (error) {
+        logger.error("Failed to connect to server", error);
+        process.exit(1);
+    }
+}
 
+startServer();
 
 //unhandled promise rejection
-process.on('unhandledRejection', (reason, promise) => {
-    logger.error('Unhandled Rejection at', promise, "reason:", reason);
-})
+
+process.on("unhandledRejection", (reason, promise) => {
+    logger.error("Unhandled Rejection at", promise, "reason:", reason);
+});
